f38e8132c57301b0a9ef75dbc50c0c8b03c8f9a0
kasigah <seth.brady@suddenlink.net>
2015-02-26 12:12:47 -0600

    Claustrophobia update

---
+++ Unity/Assets/Scripts/ChangeSize.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSize : MonoBehaviour {
+
+	//Vector3 start_size = transform.position;
+
+	// Use this for initialization
+	void Start () {
+		//Transform.position = start_size;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//GameObject cube = GameObject.Find ("Cube");
+		//Vector3 myPosition = transform.position;
+		//float scaleDir = Input.GetAxis ("Fire2");
+		//Vector3 speed = new Vector3 (0.0f, 0.0f, 0.0f);
+		//if(Transform.position.x == 0.0f) {
+		//	Transform.localPosition.z += scaleDir * 0.1f;
+		//}
+		//if (Transform.position.z == 0.0f) {
+		//	Transform.localPosition.x += scaleDir * 0.1f;
+		//}
+	
+		//transform.position = myPosition;
+	}
+}
