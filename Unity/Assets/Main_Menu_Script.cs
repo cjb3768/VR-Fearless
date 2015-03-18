cbc2c87865fc1927ea91e4e8d719358d94920431
kasigah <seth.brady@suddenlink.net>
2015-03-18 17:45:21 -0500

    loading screen
    Don't believe in yourself. Believe in the me that believes in you. Yours
    is the javascript that will pierce the internet.

---
+++ Unity/Assets/Main_Menu_Script.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu_Script : MonoBehaviour {
+	public int ButtonHeight = 50;
+	public int ButtonWidth = 150;
+	public int Spacing = 37;
+	public Texture dreams;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// GUI buttons for loading stuff
+	void OnGUI () {
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), dreams);
+
+		GUILayout.BeginArea (new Rect(Screen.width / 2 - ButtonWidth / 2, Screen.height / 2 - 200, ButtonWidth, 400));
+		if(GUILayout.Button("Begin Simulation", GUILayout.Height(ButtonHeight))){
+			//do nothing for now
+			Application.LoadLevel("Claustrophobia");
+		}
+
+		GUILayout.EndArea ();
+	}
+}
