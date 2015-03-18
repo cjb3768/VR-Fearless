6c0def744a00453b63cfd42daad0342df2e06f29
Cullen Brown <cjb3768@gmail.com>
2015-03-18 18:03:59 -0500

    Adding Audio Scripts
    Footsteps are working in the Meadow, as is some ambient background noise

---
+++ Unity/Assets/Scripts/AmbientAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientAudio : MonoBehaviour {
+
+	// Allows for call to ambient audio, on customizable randomized timer
+
+	public AudioClip sourceAudioClip;
+	public int minDelay = 1;
+	public int maxDelay = 10;
+	public int delayScalar = 44100;
+	public float audioDelay = 10.0f;
+	Random rand;
+
+	//calculate delay 
+
+	// Use this for initialization
+	void Start () {
+		audio.volume = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//audioDelay = (float) minDelay;
+		audio.clip = sourceAudioClip;
+		audio.PlayDelayed (audioDelay);
+	}
+}
--- Unity/Assets/Scripts/Footsteps.cs
+++ Unity/Assets/Scripts/Footsteps.cs
@@ -3,13 +3,67 @@ using System.Collections;
 
 public class Footsteps : MonoBehaviour {
 
+	public AudioClip myAudioClip;
+	Vector3 currentPosition;
+	enum movementState {moving, stopped};
+	movementState currentState;
+
+	/**
+	 * Play or pause audio as appropriate, based on previous and current movementStates
+	 */
+	void updateAudio (movementState previousState, movementState currentState) {
+		//player was moving
+		if (previousState == movementState.moving) {
+			//player still moving
+			if (currentState == movementState.moving) {
+				//do nothing
+			}
+			//player no longer moving
+			else {
+				audio.Stop ();
+			}
+		}
+		//player was not moving
+		else {
+			//player started moving
+			if (currentState == movementState.moving){
+				audio.Play ();
+			}
+			//player still not moving
+			else {
+				//do nothing
+			}
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		currentPosition = transform.position;
+		currentState = movementState.stopped;
+		audio.clip = myAudioClip;
+		audio.volume = 1;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//update currentPosition
+		Vector3 previousPosition = currentPosition;
+		currentPosition = transform.position;
 
+		//update currentState
+		movementState previousState = currentState;
+		//if ((currentPosition.x != currentPosition.x) || (currentPosition.z != currentPosition.z)) {
+		//if ((previousPosition.x != currentPosition.x)) {
+		if (previousPosition != currentPosition) {
+			currentState = movementState.moving;
+		}
+		else {
+			currentState = movementState.stopped;
+			//audio.Stop ();
+		}
+	
+		//update audio
+		updateAudio (previousState, currentState);
 	}
 }
