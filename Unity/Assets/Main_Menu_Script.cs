7b1d20901e8422576c73e7bade8065ced74580eb
kasigah <seth.brady@suddenlink.net>
2015-03-19 15:12:45 -0500

    tiered menu with slider
    Slider now works. Also has a back button

--- Unity/Assets/Main_Menu_Script.cs
+++ Unity/Assets/Main_Menu_Script.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class Main_Menu_Script : MonoBehaviour {
+	int mainpage = 0;
 	public int ButtonHeight = 50;
 	public int ButtonWidth = 150;
 	public int Spacing = 37;
 	public Texture dreams;
+	float hslidervalue = 1.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -19,13 +21,29 @@ public class Main_Menu_Script : MonoBehaviour {
 	// GUI buttons for loading stuff
 	void OnGUI () {
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), dreams);
+		if (mainpage == 0) {
+			GUILayout.BeginArea (new Rect (Screen.width / 2 - ButtonWidth / 2, Screen.height / 2 - 200, ButtonWidth, 400));
+			if (GUILayout.Button ("Begin Simulation", GUILayout.Height (ButtonHeight))) {
+				//do nothing for now
+				mainpage = 1;
+			}
 
-		GUILayout.BeginArea (new Rect(Screen.width / 2 - ButtonWidth / 2, Screen.height / 2 - 200, ButtonWidth, 400));
-		if(GUILayout.Button("Begin Simulation", GUILayout.Height(ButtonHeight))){
-			//do nothing for now
-			Application.LoadLevel("Claustrophobia");
+			GUILayout.EndArea ();
 		}
+		if (mainpage == 1) {
+			GUILayout.BeginArea (new Rect (Screen.width / 2 - ButtonWidth / 2, Screen.height / 2 - 200, ButtonWidth, 400));
+			hslidervalue = GUILayout.HorizontalSlider(hslidervalue, 1.0f, 2.0f);
+			if (GUILayout.Button ("Begin Simulation", GUILayout.Height (ButtonHeight))) {
+				//do nothing for now
 
-		GUILayout.EndArea ();
-	}
+				Application.LoadLevel ((int)hslidervalue);
+			}
+			if(GUILayout.Button("Back", GUILayout.Height(ButtonHeight))) {
+				mainpage = 0;
+			}
+			
+			GUILayout.EndArea ();
+
+		}
+}
 }
