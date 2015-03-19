b4ca069c5c8692675c14b5207adef6eba15281bb
Cullen Brown <cjb3768@gmail.com>
2015-03-19 12:03:50 -0500

    More Audio
    Script for playing randomly scheduled audio complete; now to collect
    ambient sounds. Also, began script for event based audio.

---
+++ Unity/Assets/Scripts/EventAudio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventAudio : MonoBehaviour {
+
+	public AudioClip sourceAudioClip;
+	//Vector3 
+
+	// Use this for initialization
+	void Start () {
+		audio.volume = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
