a16786d2872e39341d5d32d595875bc412acc0f6
Cullen Brown <cjb3768@gmail.com>
2015-03-19 15:51:24 -0500

    Added some Gui Scripts
    The therapist gui works somewhat; needs to be made prettier and we need
    to render it on its own screen. Also worked on gui for user

---
+++ Unity/Assets/Scripts/DisplayHeartRateStatistics.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayHeartRateStatistics : MonoBehaviour {
+	
+	//Base script for showing heart rate info; need to connect biometrics to this
+	
+	public Text heartRateText;
+	public Text heartRateDeviationText;
+	public string heartRateInfo = "No Information Yet";
+	public string heartRateDeviationInfo = "+ 0 bpm";
+	public int updateDelay = 10;
+	public float baselineHeartRate = 90f;
+	private int updateCounter = 0;
+	
+	string updateHeartRateInfo (float currentHeartRate){
+		//will ultimately show accurate heart rate information; for now, just shows whatever double is passed in
+		return "Heart Rate: " + currentHeartRate + " bpm";
+	}
+	
+	string updateHeartRateDeviationInfo (float currentHeartRate, float baselineHeartRate){
+		float deviation = baselineHeartRate - currentHeartRate;
+		string deviationInfo = "Delta Heart Rate: ";
+		
+		if (deviation < 0) {
+			deviationInfo += "-" + deviation + " bpm";
+		} 
+		else if (deviation > 0) {
+			deviationInfo += "+" + deviation + " bpm";
+		} 
+		else {
+			deviationInfo += deviation + " bpm";
+		}
+
+		return deviationInfo;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		heartRateText.text = heartRateInfo;
+		heartRateDeviationText.text = heartRateDeviationInfo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//here is where we will call other scripts to read in real heart rate information; for now, we are just going to do random numbers between 80 and 100 on a delay
+		updateCounter ++;
+		if (updateCounter == updateDelay) {
+			float currentHeartRate = Random.Range (80, 100);
+			heartRateInfo = updateHeartRateInfo (currentHeartRate);
+			heartRateText.text = heartRateInfo;
+			heartRateDeviationInfo = updateHeartRateDeviationInfo(currentHeartRate, baselineHeartRate);
+			heartRateDeviationText.text = heartRateDeviationInfo;
+			updateCounter = 0;
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayTime : MonoBehaviour {
+
+	public Text currentTimeDisplay;
+	public string currentTimeString = "00:00:00";
+
+	string formatCurrentTime (float currentTime){
+		int integerTime = (int) currentTime;
+		int hrs = integerTime / 3600;
+		int mins = integerTime / 60;
+		int secs = integerTime % 60;
+		string formattedTime = string.Format ("Current time: {0:00}:{1:00}:{2:00}", hrs, mins, secs);
+		return formattedTime;
+	}
+
+	string updateCurrentTime (){
+		float currentTime = Time.realtimeSinceStartup;
+		return formatCurrentTime(currentTime);
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentTimeDisplay.text = currentTimeString;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTimeString = updateCurrentTime ();
+		currentTimeDisplay.text = currentTimeString;
+	}
+}
