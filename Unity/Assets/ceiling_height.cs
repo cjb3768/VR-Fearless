4d84c1c72f7f20116c893a286b820607b964be41
kasigah <seth.brady@suddenlink.net>
2015-03-31 14:02:55 -0500

    Adaptation work
    The ceiling moves!

---
+++ Unity/Assets/ceiling_height.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ceiling_height : MonoBehaviour {
+	float height = 3.5f;
+	// Use this for initialization
+	void Start () {
+		Vector3 initpos = new Vector3(this.transform.position.x, height, this.transform.position.z); 
+		this.transform.position = initpos;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetAxis ("Fire1") < 0.0f) {
+			if (this.transform.position.y > 2.0f) {
+				Vector3 pos = this.transform.position;
+				pos.y = pos.y - (0.005f);
+				this.transform.position = pos;
+			}
+		}
+		if (Input.GetAxis ("Fire1") > 0.0f) {
+			if (this.transform.position.y < 6.0f) {
+				Vector3 pos = this.transform.position;
+				pos.y = pos.y + (0.005f);
+				this.transform.position = pos;
+			}
+		}
+	}
+}
