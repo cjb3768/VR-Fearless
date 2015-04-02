fa3294596fd917590b1372a926eb64b5d7dfff6d
kasigah <seth.brady@suddenlink.net>
2015-04-02 14:12:56 -0500

    I have a button!
    But it isn't working

---
+++ Unity/Assets/Scripts/end_sim_button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class end_sim_button : MonoBehaviour {
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetButton
+		Ray myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(myray,out hit, 5.0f)) {
+			Application.LoadLevel (0);
+		}
+	}
+
+	void OnMouseUp () {
+
+	}
+}
