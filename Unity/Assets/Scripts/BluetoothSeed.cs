7f7f55bb39be86746e86e00e3131c05f76ae10b9
Cullen Brown <cjb3768@gmail.com>
2015-04-03 19:46:56 -0500

    more bluetooth
    added method to reverse packets; we can use this to handle changes in
    endianness both with read and written packets

--- Unity/Assets/Scripts/BluetoothSeed.cs
+++ Unity/Assets/Scripts/BluetoothSeed.cs
@@ -12,16 +12,24 @@ public class BluetoothSeed : MonoBehaviour {
 	//public string receivedData = "";
 	public int ticksTillNextMessage = 2;
 
-	//here are the packets we currently need to send to the device; may need to convert to match endianness
-	public byte[] lifesignMessage = {0x02, 0x23, 0x00, 0x00, 0x03};
-	public byte[] invertedLifesignMessage = {0x03, 0x00, 0x00, 0x23, 0x02};
-	public byte[] summaryPacketsStartRequest = {0x02, (byte)0xBD , 0x02, 0x01, 0x00, 0x00, 0x03};
-	public byte[] summaryPacketsStopRequest = {0x02, (byte)0xBD , 0x02, 0x00, 0x00, 0x00, 0x03};
-
 	private static byte STX = 0x02;
 	private static byte ETX = 0x03;
 	private static byte ACK = 0x06;
 	private static byte NAK = 0x15;
+
+	//here are the packets we currently need to send to the device; may need to convert to match endianness
+	public static byte[] lifesignMessage = {0x02, 0x23, 0x00, 0x00, 0x03};
+	//public static byte[] lifesignMessage = {ETX, 0x00, 0x00, 0x23, STX};
+	//public static byte[] summaryPacketsStartRequest = {0x02, (byte)0xBD , 0x02, 0x01, 0x00, 0x00, 0x03};
+	public static byte[] summaryPacketsStartRequest = {STX, (byte)0xBD , 0x02, 0x01, 0x00, 0x00, ETX};
+	//public static byte[] summaryPacketsStartRequest = {0x03, 0x00 , 0x00, 0x01, 0x02, (byte)0xBD, 0x02};
+	//public static byte[] summaryPacketsStartRequest = {ETX, 0x00 , 0x00, 0x01, 0x02, (byte)0xBD, STX};
+	//public static byte[] summaryPacketsStopRequest = {0x02, (byte)0xBD , 0x02, 0x00, 0x00, 0x00, 0x03};
+	public static byte[] summaryPacketsStopRequest = {STX, (byte)0xBD , 0x02, 0x00, 0x00, 0x00, ETX};
+	//public static byte[] summaryPacketsStopRequest = {0x03, 0x00 , 0x00, 0x00, 0x02, (byte)0xBD, 0x02};
+	//public static byte[] summaryPacketsStopRequest = {ETX, 0x00 , 0x00, 0x00, 0x02, (byte)0xBD, STX};
+	public static byte[] btLinkConfigPacket = {STX, (byte) 0xA4, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, ETX}; 
+	//public static byte[] btLinkConfigPacket = {ETX, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, (byte)0xA4, STX};
 
 	private static int CRC8_POLY = 0x8C;
 	public CRC8 crc = new CRC8(CRC8_POLY);
@@ -32,12 +40,13 @@ public class BluetoothSeed : MonoBehaviour {
 		//send lifesign message
 		//serial.Write (lifesignMessage, 0, 5);
 		//writeToConnection (lifesignMessage);
-		//writeToConnection (invertedLifesignMessage);
+		//writeToConnection (summaryPacketsStartRequest);
+		//writeToConnection (btLinkConfigPacket);
 	}
 
 	void Update() { 
 
-		InvokeRepeating ("sendLifesign", 0, 2);
+		InvokeRepeating ("sendLifesign", 0, 8);
 		//InvokeRepeating ("writeToConnection(lifesignMessage)", 0, 1);
 		/*
 		//int mess = serial.ReadByte ();
@@ -101,25 +110,52 @@ public class BluetoothSeed : MonoBehaviour {
 		}
 	}
 
+	public void reversePacket(byte[] sourcePacket, byte[] reversedPacket){
+		int length = sourcePacket.Length;
+		//byte[] rPacket = new byte[length];
+		for (int i = 0; i < length; i++){
+			reversedPacket[i] = sourcePacket[length-(i+1)];
+		}
+	}
+
 
 	public void sendLifesign(){
-		serial.Write (invertedLifesignMessage, 0, invertedLifesignMessage.Length);
+		Debug.Log ("in function lifesign message length = " + lifesignMessage.Length + "\n");
+		//serial.Write (lifesignMessage, 0, lifesignMessage.Length);
+		writeToConnection (lifesignMessage);
 	}
 	/**
 	 * Write a passed in packet to the serial port
 	 */
 	public void writeToConnection(byte[] packet){
+		int packetLength = packet.Length;
+		byte[] reversedPacket = new byte[packetLength];
+		Debug.Log ("packetlength = " + packetLength + "\n");
 		//determine packet type by message id
 		if (packet [1] == 0x23) {
 			//lifesign
-			serial.Write (packet, 0, packet.Length);
-		}
-		else if (packet[1] == (byte) 0xBD){
+			reversePacket (packet, reversedPacket);
+			Debug.Log ("size of newPacket = " + reversedPacket.Length + "\n");
+			serial.Write (reversedPacket, 0, reversedPacket.Length);
+		} 
+		else if (packet [1] == (byte)0xBD) {
 			//summary packet start or stop request
 			byte[] crcCalc = new byte[2];
-			Array.Copy(packet, 3, crcCalc, 0, 2);
-			packet[5] = crc.Calculate(crcCalc);
-			serial.Write (packet, 0, packet.Length);
+			Array.Copy (packet, 3, crcCalc, 0, 2);
+			packet [5] = crc.Calculate (crcCalc);
+			reversePacket (packet, reversedPacket);
+			Debug.Log ("size of newPacket = " + reversedPacket.Length + "\n");
+			serial.Write (reversedPacket, 0, reversedPacket.Length);
+		} 
+		else if (packet [1] == (byte)0xA4) {
+			//bt config set packet
+			byte[] crcCalc = new byte[4];
+			Array.Copy (packet, 3, crcCalc, 0, 4);
+			packet [7] = crc.Calculate (crcCalc);
+			reversePacket (packet, reversedPacket);
+			Debug.Log ("size of newPacket = " + reversedPacket.Length + "\n");
+			serial.Write (reversedPacket, 0, reversedPacket.Length);
+			//serial.Write (packet, 0, packet.Length);
 		}
 	}
 	
