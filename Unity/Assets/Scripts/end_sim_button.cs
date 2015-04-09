8fe1cde07bd51c3457c3f5f8be2cba9129446d30
kasigah <seth.brady@suddenlink.net>
2015-04-09 14:17:17 -0500

    Button!

--- Unity/Assets/Scripts/end_sim_button.cs
+++ Unity/Assets/Scripts/end_sim_button.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class end_sim_button : MonoBehaviour {
-	RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
@@ -11,14 +10,10 @@ public class end_sim_button : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetButton
-		Ray myray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(myray,out hit, 5.0f)) {
-			Application.LoadLevel (0);
-		}
+
 	}
 
-	void OnMouseUp () {
-
+	void OnTriggerEnter(Collider other) {
+		Application.LoadLevel (0);
 	}
 }
