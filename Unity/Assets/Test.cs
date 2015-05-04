b9bdae0badd72f099f40eafe567a2a77f95bb7ab
hainesricky <rhaines@imac-d25hm0q5dhjw.cs.tamu.edu>
2015-05-03 19:11:59 -0500

    WEKA stuff
    Weka dlls and other necessary things. A basic script (Test) shows how
    to read in a file, read in a model, and then classify an instance.

---
+++ Unity/Assets/Test.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using weka;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Testing...");
+		classifyTest ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void classifyTest()
+	{
+
+		//Debug.Log ("Attempting to read file...");
+		//Read a .arff for usage
+		weka.core.Instances insts = new weka.core.Instances(new java.io.FileReader("ConvertedLogs/3.arff"));
+
+		//Debug.Log ("Attempting to set class index...");
+
+		insts.setClassIndex(insts.numAttributes() - 1);
+
+		//Debug.Log ("Loading classifier...");
+
+		//Specify model to be used here
+		//A model is a pre-trained 
+		weka.classifiers.Classifier kstar = (weka.classifiers.Classifier) weka.core.SerializationHelper.read ("Models/kstar.model");
+		//Debug.Log ("Performing KStar evaluation");
+
+		//Perform the evaluation on the .arff specified
+		//weka.classifiers.Evaluation dataTest = new weka.classifiers.Evaluation(insts);
+		//dataTest.evaluateModel(kstar,insts);
+
+		weka.core.Instance topInst = insts.instance (0);
+		double predictedClass = kstar.classifyInstance (topInst);
+
+		//The prediction for the top instance of data.
+		Debug.Log (predictedClass);
+
+
+	}
+
+
+
+
+}
